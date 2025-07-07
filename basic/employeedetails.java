c91a1e9f3e8a0c6ac94102eff1eed5ac0c951748
123-varshini <varshinijothi17@gmail.com>
2025-07-07 10:59:23 +0530

    firstprogram

---
+++ basic/employeedetails.java
@@ -0,0 +1,21 @@
+public class employeedetails {
+    public static void main(String[] args) {
+        String[][] employees = new String[10][2];
+        {
+            employees[0][0] = "15000";
+            employees[0][1] = "thara";
+            employees[1][0] = "20000";
+            employees[1][1] = "thara";
+            employees[2][1] = "25000";
+            employees[2][2] = "thara";
+            System.out.println("EmployeeDetails:");
+            for(int i = 0; i < employees.length; i++) {
+                System.out.println("salary:"+ employees[i][0]+"Name:" + employees[i][1]);
+            }
+            
+        }
+    }
+}
+
+
+
