3bcaa2742a2a57de81abd71ff250c94b4797235f
varjinis <varjinis1705@gmail.com>
2025-07-07 14:33:21 +0530

    StringBuilderexample

---
+++ basic/StringBuilderexample.java
@@ -0,0 +1,16 @@
+public class StringBuilderexample {
+   public static void main(String[] args) {
+         StringBuilder SB = new StringBuilder();
+SB.append("Java");
+        SB.append("is ");
+        SB.append("the ");
+        SB.append("most ");
+        SB.append("used ");
+        SB.append("language ");
+        SB.append("for ");
+        SB.append("enterprise ");
+        SB.append("applications ");
+        System.out.println("After appends: " + SB); 
+
+   } 
+}
